86c05a547c62f1568609d678504ba525a66f8353
saracat2012 <saracat2012@gmail.com>
2026-02-13 15:27:15 -0500

    logon screen theme and layout set. Light/dark themes created and working

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LogonScreen } from './logon-screen/logon-screen';
-
 
 @Component({
   selector: 'my-root',
   standalone: true,
   imports: [MatToolbarModule, LogonScreen],
   template: `
-    <mat-toolbar color="primary">PharmaStock</mat-toolbar>
+    <mat-toolbar color="primary">
+      PharmaStock
+      <span style="flex: 1 1 auto;"></span>
+      <button mat-button (click)="toggleTheme()">Toggle Theme</button>
+    </mat-toolbar>
+
     <app-logon-screen></app-logon-screen>
   `
 })
-export class App {}
+export class App implements OnInit {
+
+  ngOnInit() {
+    document.body.classList.add('light-theme'); // default theme
+  }
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+  }
+}
