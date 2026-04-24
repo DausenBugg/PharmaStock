3cc536c7056f1fcbc35588060e08c4b2a3fc16fe
Dausen Bugg <DVBugg@student.fullsail.edu>
2026-04-24 06:44:43 -0500

    fixed syntax error.

--- PharmaStock/Data/PharmaStockDBContext.cs
+++ PharmaStock/Data/PharmaStockDBContext.cs
@@ -154,6 +154,7 @@ namespace PharmaStock.Data
                     .HasForeignKey<UserProfile>(p => p.UserId)
                     .HasPrincipalKey<IdentityUser>(u => u.Id)
                     .OnDelete(DeleteBehavior.Cascade);
+            });
             // Seed default notification settings (single-row config)
             modelBuilder.Entity<NotificationSetting>().HasData(new NotificationSetting
             {
